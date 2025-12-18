d3b36b00549a66013f51b4fd71fab2905344dfc6
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-12-18 20:38:06 +0530

    fixed

--- java-internship-pr-practice/src/main/java/com/internship/App.java
+++ java-internship-pr-practice/src/main/java/com/internship/App.java
@@ -3,5 +3,8 @@ package com.internship;
 public class App {
     public static void main(String[] args) {
         System.out.println("Java Internship PR Practice Repo");
+        System.out.println("Testing StringUtils:");
+        System.out.println(StringUtils.reverse("hello")); 
+        System.out.println(StringUtils.reverse("Java"));
     }
 }
