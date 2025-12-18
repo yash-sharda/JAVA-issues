c32d1b9e358ba2e4e8ef4c0e60a60e157a4fd8de
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-12-18 20:43:51 +0530

    arrayutils

--- java-internship-pr-practice/src/main/java/com/internship/App.java
+++ java-internship-pr-practice/src/main/java/com/internship/App.java
@@ -4,7 +4,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Java Internship PR Practice Repo");
         System.out.println("Testing StringUtils:");
-        System.out.println(StringUtils.reverse("hello")); 
+        System.out.println(StringUtils.reverse("hello"));
         System.out.println(StringUtils.reverse("Java"));
+        System.out.println("Testing ArrayUtils:");
+        int[] numbers = { 5, 2, 9, 1, 7 };
+        System.out.println("Max: " + ArrayUtils.findMax(numbers));
     }
 }
