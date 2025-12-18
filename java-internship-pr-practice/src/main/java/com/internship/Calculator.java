aee49b918235a49a5382b187b8c2527b91fc3565
Yash-Supekar <yashsupekar500@gmail.com>
2025-12-18 20:59:41 +0530

    FInal change in features

--- java-internship-pr-practice/src/main/java/com/internship/Calculator.java
+++ java-internship-pr-practice/src/main/java/com/internship/Calculator.java
@@ -2,12 +2,16 @@ package com.internship;
 
 public class Calculator {
 
+
     public int add(int a, int b) {
-        return a + b;
+        System.out.println(a + b);
+        return 0;
     }
 
     // ISSUE: Implement subtract method
     public int subtract(int a, int b) {
-        return a - b;
+        System.out.println(a-b);
+        
+        return 0;
     }
 }
